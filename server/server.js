2ca75436e83ad00ab1f3481740e7aa7d5019e551
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2022-04-12 15:12:45 +0800

    setup basic server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,10 @@
-console.log("test");
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.listen(PORT, (req, res) => {
+	console.log(`Server is running on port ${PORT}`);
+});
